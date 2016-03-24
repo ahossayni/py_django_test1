515b4a9b2b4f9d8a46da45770b6d2f58ca8e4a6a
Azeddine Hossayni <azeddine.hossayni@gmail.com>
2016-03-24 15:46:30 +0100

    Ajout des fichiers du projet

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\ahossayni\documents\perso\projects\py_django_tests\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
